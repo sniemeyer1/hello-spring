b07b1bbaeb0584095ac6f138ce1a7594fbcf82c4
Savannah Niemeyer <savannahniemeyer@gmail.com>
2020-02-18 03:46:27 -0600

    working again

--- src/main/java/org/launchcode/hellospring/controllers/HelloController.java
+++ src/main/java/org/launchcode/hellospring/controllers/HelloController.java
@@ -5,57 +5,66 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
-@RequestMapping("hello")
+//@RequestMapping("hello")
 
 public class HelloController {
-    private static String language;
-
+    //private static String language;
+//handles requests at path /hello
 //    @GetMapping("hello")
 //    @ResponseBody
 //    public String hello(){
 //        return "Hello, Spring!!";
 //    }
 
-//    @GetMapping("goodbye")
-//    public String goodbye(){
-//        return "BYEBYE, Spring!!";
-//    }
-
-    //responds to /hello?name=Launchcode
-    @RequestMapping(method = {RequestMethod.GET, RequestMethod.POST})
+    @GetMapping("goodbye")
     @ResponseBody
-    public String hello(@RequestParam String name){
+    public String goodbye(){
+        return "BYEBYE, Spring!!";
+    }
+
+    //responds to /hello?name=Launchcode handles query parameters to create a gretting that is custom to the request
+   // @RequestMapping(method = {RequestMethod.GET, RequestMethod.POST})
+    @GetMapping("hello")
+    @ResponseBody
+    public String helloWithQueryParam(@RequestParam String name){
         return "Hello, " + name + "!!!!";
     }
-    // responds to /hello/Launchcode
-    @GetMapping("hello/{name}")
-    @ResponseBody
-    public String helloAgain(@PathVariable String name){
-        return "Hi, " + name + "!";
-    }
+// create get request handlers that accept data, dynamic handler
+    // the data is part of the path, not the query string
+//    responds to /hello/Launchcode
 
-//    @RequestMapping(method = {RequestMethod.GET, RequestMethod.POST})
-//    public static String createMessage(@RequestParam String name, String language){
-//        String englishGreeting = "Hello ";
-//        String italianGreeting = "Ciao ";
-//        String frenchGreeting = "Bonjour ";
-//        String germanGreeting = "Hallo ";
-//        String spanishGreeting = "Hola ";
-//
-//        switch (language) {
-//            case "French":
-//                return frenchGreeting + name + "!";
-//            case "Spanish":
-//                return spanishGreeting + name + "!";
-//            case "German":
-//                return germanGreeting + name + "!";
-//            case "Italian":
-//                return italianGreeting + name + "!";
-//            default:
-//                return englishGreeting + name + "!";
-//        }
-//
+    //PV tells sb that this is handler method is looking for hello/whatevername
+//    @GetMapping("hello/{name}")
+//    @ResponseBody
+//    public String helloWithPathParam(@PathVariable String name){
+//        return "Hi, " + name + "!";
 //    }
+
+    //@GetMapping("hello/{name}")
+    @RequestMapping(method = {RequestMethod.GET, RequestMethod.POST}, value= "hello")
+    @ResponseBody
+    public static String createMessage(@RequestParam String name, String language){
+        String greeting;
+
+        if ("French".equals(language)) {
+            greeting = "Bonjour ";
+        }else if("Italian".equals(language)){
+            greeting = "Ciao ";
+        }else if("German".equals(language)){
+            greeting = "Hallo ";
+        }else if("Spanish".equals(language)){
+            greeting = "Hola ";
+        }
+        else{
+            greeting = "Hello ";
+        }
+        return "<html>" +
+                "<body>" +
+                "<h2 style='color: red;'>" +
+                greeting + name + "!" + "</h2>" +
+                "</body>" +
+                "</html>";
+    }
 
 
 
@@ -68,5 +77,5 @@ public class HelloController {
     public String helloForm(){
         return "form";
     }
-
+//
 }
