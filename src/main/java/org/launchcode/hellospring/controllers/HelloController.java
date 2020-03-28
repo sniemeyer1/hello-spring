8c1c9df70099e6c4f465575375abc1aa68ef6687
Savannah Niemeyer <savannahniemeyer@gmail.com>
2020-03-27 23:53:34 -0500

    some practice with thymeleaf

--- src/main/java/org/launchcode/hellospring/controllers/HelloController.java
+++ src/main/java/org/launchcode/hellospring/controllers/HelloController.java
@@ -2,9 +2,14 @@ package org.launchcode.hellospring.controllers;
 
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Controller
+//@ResponseBody
 //@RequestMapping("hello")
 
 public class HelloController {
@@ -14,68 +19,85 @@ public class HelloController {
 //    @ResponseBody
 //    public String hello(){
 //        return "Hello, Spring!!";
-//    }
+////    }
 
-    @GetMapping("goodbye")
-    @ResponseBody
-    public String goodbye(){
-        return "BYEBYE, Spring!!";
-    }
-
-    //responds to /hello?name=Launchcode handles query parameters to create a gretting that is custom to the request
-   // @RequestMapping(method = {RequestMethod.GET, RequestMethod.POST})
-    @GetMapping("hello")
-    @ResponseBody
-    public String helloWithQueryParam(@RequestParam String name){
-        return "Hello, " + name + "!!!!";
-    }
-// create get request handlers that accept data, dynamic handler
-    // the data is part of the path, not the query string
-//    responds to /hello/Launchcode
-
-    //PV tells sb that this is handler method is looking for hello/whatevername
-//    @GetMapping("hello/{name}")
 //    @ResponseBody
-//    public String helloWithPathParam(@PathVariable String name){
-//        return "Hi, " + name + "!";
+//    @GetMapping("goodbye")
+//    public String goodbye(){
+//        return "BYEBYE, Spring!!";
 //    }
 
-    //@GetMapping("hello/{name}")
-    @RequestMapping(method = {RequestMethod.GET, RequestMethod.POST}, value= "hello")
-    @ResponseBody
-    public static String createMessage(@RequestParam String name, String language){
-        String greeting;
+    //responds to /hello?name=Launchcode handles query parameters to create a greeting that is custom to the request
+    //@ResponseBody
+    @RequestMapping(value = "hello", method = {RequestMethod.GET, RequestMethod.POST})
+    public static String helloWithQueryParam(@RequestParam String myname, Model model) {
+        String greeting = "hiya doofus" + myname + "!";
+        model.addAttribute("greetingHello", greeting);
+        return "hello";
+    }
 
-        if ("French".equals(language)) {
-            greeting = "Bonjour ";
-        }else if("Italian".equals(language)){
-            greeting = "Ciao ";
-        }else if("German".equals(language)){
-            greeting = "Hallo ";
-        }else if("Spanish".equals(language)){
-            greeting = "Hola ";
-        }
-        else{
-            greeting = "Hello ";
-        }
-        return "<html>" +
-                "<body>" +
-                "<h2 style='color: red;'>" +
-                greeting + name + "!" + "</h2>" +
-                "</body>" +
-                "</html>";
+     //create get request handlers that accept data, dynamic handler
+     //    the data is part of the path, not the query string
+     //   responds to /hello/Launchcode
+
+     //   PV tells sb that this is handler method is looking for hello/whatevername
+//        @GetMapping("{name}")
+//        public String helloWithPathParam(@PathVariable String name){
+//            return "Hi, " + name + "!";
+//        }
+//
+//     @ResponseBody
+//     @RequestMapping(method = {RequestMethod.GET, RequestMethod.POST}, value= "{name}")
+//    public static String createMessage(@RequestParam String name, String language){
+//        String greeting;
+//        if ("French".equals(language)) {
+//            greeting = "Bonjour ";
+//        }else if("Italian".equals(language)){
+//            greeting = "Ciao ";
+//        }else if("German".equals(language)){
+//            greeting = "Hallo ";
+//        }else if("Spanish".equals(language)){
+//            greeting = "Hola ";
+//        }
+//        else{
+//            greeting = "Hello ";
+//        }
+//        return "<html>" +
+//                "<body>" +
+//                "<h2 style='color: red;'>" +
+//                greeting + name + "!" + "</h2>" +
+//                "</body>" +
+//                "</html>";
+//    }
+    @GetMapping("hello-names")
+    public String helloNames(Model model) {
+        List<String> names = new ArrayList<>();
+        names.add("LaunchCode");
+        names.add("Java");
+        names.add("JavaScript");
+        model.addAttribute("names", names);
+        return "hello-list";
     }
 
 
-
-//    @RequestMapping(method = {RequestMethod.GET, RequestMethod.POST})
-//    public String hello(@RequestParam String name) {
-//        return "Hiya, " + name + "!";
-//    }
-
-    @GetMapping("form")
+    @GetMapping("theform")
     public String helloForm(){
         return "form";
+//        return "<html>" +
+//                "<body>" +
+//                "<form action='hello' method='post'>" +
+//                "<input type='text' name='name'>" +
+//                "<select name = 'language' id='language'>" +
+//                "<option value='English'>English</option>" +
+//                "<option value='Spanish'>Spanish</option>" +
+//                "<option value='Italian'>Italian</option>" +
+//                "<option value='French'>French</option>" +
+//                "<option value='German'>German</option>" +
+//                "</select>" +
+//                "<input type='submit' value='Greet Me!'>" +
+//               "</form>" +
+//                "</h2>" +
+//                "</body>" +
+//                "</html>";
     }
-//
 }
